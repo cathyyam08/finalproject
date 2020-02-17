0bf1e51c0d77aa6b8491539aa9fe1e76422477a9
cathyyam08 <cyam@estee.com>
2020-02-17 12:46:56 -0500

    First push to github

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    slidesToShow: 3,
+    prevArrow: '<img class="slick-prev" src="images/leftarrow.png">',
+    nextArrow: '<img class="slick-next" src="images/rightarrow.png">',
+    fade: true,
+  });
+});
