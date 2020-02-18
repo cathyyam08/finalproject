205cac40a7eec29453a485d675e833263764bcef
cathyyam08 <cyam@estee.com>
2020-02-17 21:01:00 -0500

    Changed carousel and CSS

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ $(document).ready(function() {
     slidesToShow: 3,
     prevArrow: '<img class="slick-prev" src="images/leftarrow.png">',
     nextArrow: '<img class="slick-next" src="images/rightarrow.png">',
-    fade: true,
   });
 });
