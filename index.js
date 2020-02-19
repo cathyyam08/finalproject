3d740c60b9a0c9048567bc5bf986644bd64bef11
cathyyam08 <cyam@estee.com>
2020-02-18 21:26:01 -0500

    Changed it to scroll 1 slide instead of 3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $('.carousel').slick({
       breakpoint: 1024,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         infinite: true,
         dots: true
       }
